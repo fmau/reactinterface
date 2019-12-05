bb8b5636e11028589081431aaf11846e6b7cadd9
fmau <florence@maurice-web.de>
2019-12-05 16:14:42 +0100

    Delete SearchAppointments.js

--- src/components/SearchAppointments.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class SearchAppointments extends Component {
-    render() {
-        return <div>Termine suchen </div>
-    }
-}
-
-export default SearchAppointments;
