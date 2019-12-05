4af5dc97ae90390571a6bef402021763c0e6a91c
fmau <florence@maurice-web.de>
2019-12-05 16:08:31 +0100

    Add files via upload

---
+++ src/components/App-class.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+               <div>Termine hinzufügen</div>
+               <div>Termine suchen</div>
+               <div>Termine auflisten</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+function App() {
+  return (
+    <main className="page bg-white" id="petratings">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 bg-white">
+          <div className="container">
+            <AddAppointments />
+            <SearchAppointments />
+            <ListAppointments />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+   
+  );
+}
+
+export default App;
