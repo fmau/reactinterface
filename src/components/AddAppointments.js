95a5ee993986f42f0fd95d97d233afe57ef038a6
fmau <florence@maurice-web.de>
2019-12-05 16:14:07 +0100

    Delete AddAppointments.js

--- src/components/AddAppointments.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class AddAppointments extends Component {
-    render() {
-        return <div>Add Appointments </div>
-    }
-}
-
-export default AddAppointments;
