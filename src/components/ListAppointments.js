ef1abece3230cd444b3c708a04688e03447294e5
fmau <florence@maurice-web.de>
2019-12-07 16:20:56 +0100

    Add files via upload

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ListAppointments extends Component {
-    render() {
-        const listItems = this.props.appointments.map(item => (
-            <div>
-             <div>{item.petName}</div>
-             <div>{item.ownerName}</div>
-             </div>
-          ) );
-        return <div>{listItems} </div>
-    }
+  render() {
+    
+    return (
+      <div className="appointment-list item-list mb-3">
+          {this.props.appointments.map(item => (
+        <div className="pet-item col media py-3">
+          <div className="mr-3">
+            <button className="pet-delete btn btn-sm btn-danger">X</button>
+          </div>
+
+          <div className="pet-info media-body">
+            <div className="pet-head d-flex">
+              <span className="pet-name">{item.petName}</span>
+              <span className="apt-date ml-auto">{item.aptDate}</span>
+            </div>
+
+            <div className="owner-name">
+              <span className="label-item">Besitzer: </span>
+              <span>{item.ownerName}</span>
+            </div>
+            <div className="apt-notes">{item.aptNotes}</div>
+          </div>
+        </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ListAppointments;
