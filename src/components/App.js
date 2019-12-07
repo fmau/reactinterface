825a659e0b1c69483665eb66a184e331722aac68
fmau <florence@maurice-web.de>
2019-12-07 10:28:30 +0100

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,53 @@
-import React, {Component} from 'react';
-import '../css/App.css';
-import AddAppointments from './AddAppointments';
-import SearchAppointments from './SearchAppointments';
-import ListAppointments from './ListAppointments';
+import React, { Component } from "react";
+import "../css/App.css";
+import AddAppointments from "./AddAppointments";
+import SearchAppointments from "./SearchAppointments";
+import ListAppointments from "./ListAppointments";
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
-    this.state= {
-      
+    this.state = {
       myAppointments: []
-    }
+    };
   }
-  componentDidMount () {
-    fetch('./data.json')
-    .then(response => response.json())
-    .then(result => {
-      const apts = result.map(item => {
-        return item;
-      })
-      this.setState({
-        myAppointments: apts
+  componentDidMount() {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
       });
-    });
-    
   }
 
   render() {
-  return (
-    <main className="page bg-white" id="petratings">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 bg-white">
-          <div className="container">
-           
-            <AddAppointments />
-            <SearchAppointments />
-            <ListAppointments />
+    const listItems = this.state.myAppointments.map(item => (
+      <div>
+       <div>{item.petName}</div>
+       <div>{item.ownerName}</div>
+       </div>
+    ) );
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+               {listItems}
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </main>
-   
-  );
-}
+      </main>
+    );
+  }
 }
 
 export default App;
