10c57e92fe70d12e612f6e978f3e1797fab067b1
fmau <florence@maurice-web.de>
2019-12-14 19:00:34 +0100

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AddAppointments from "./AddAppointments";
 import SearchAppointments from "./SearchAppointments";
 import ListAppointments from "./ListAppointments";
 
-import { without } from "lodash";
+import { without, findIndex } from "lodash";
 
 class App extends Component {
   constructor() {
@@ -22,6 +22,7 @@ class App extends Component {
     this.addAppointment = this.addAppointment.bind(this);
     this.changeOrder = this.changeOrder.bind(this);
     this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
   }
   toggleForm () {
     this.setState({
@@ -49,7 +50,16 @@ class App extends Component {
       orderDir: dir
     })
   }
-
+  updateInfo(name, value, id) {
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
 
   deleteAppointment(apt) {
     let tempApts = this.state.myAppointments;
@@ -126,6 +136,7 @@ class App extends Component {
                 <ListAppointments
                   appointments={filteredApts}
                   deleteAppointment={this.deleteAppointment}
+                  updateInfo={this.updateInfo}
                 />
               </div>
             </div>
